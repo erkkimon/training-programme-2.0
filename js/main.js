70cd8f4c8a61eb37b3df322556a6c0428d25be8c
Tom <Tom@Airi-Petteri-2.local>
2014-06-11 20:56:03 +0300

    Back handling test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var gaPlugin;
 
 document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("backbutton", onBackKeyDown, false);
 
 function onDeviceReady() 
 {
@@ -8,6 +9,10 @@ function onDeviceReady()
   gaPlugin.init(successHandler, errorHandler, "UA-51549024-1", 1);
   gaPlugin.trackPage(nativePluginResultHandler, nativePluginErrorHandler, "index.html");
   gaPlugin.exit(nativePluginResultHandler, nativePluginErrorHandler);
+}
+
+function onBackKeyDown() {
+    alert("back pressed");
 }
 
 function successHandler() // for gaPlugin
